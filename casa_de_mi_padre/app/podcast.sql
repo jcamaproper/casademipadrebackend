e5d9a20cda3f8bcf31a3086dbd55d4b29d68c9b6
jcamacho <juan.camacho@proper.ai>
2024-01-14 18:20:29 -0600

    Return pages, data structure

--- casa_de_mi_padre/app/podcast.sql
+++ casa_de_mi_padre/app/podcast.sql
@@ -2,5 +2,7 @@ CREATE TABLE IF NOT EXISTS PODCAST (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     podcast_uri TEXT,
     devocional_id UUID REFERENCES devocionales(id),
-    fecha DATE
+    fecha DATE,
+    tema TEXT,
+    descripcion TEXT
 );
--- casa_de_mi_padre/app/trivia.sql
+++ casa_de_mi_padre/app/trivia.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS trivia (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     trivia JSONB,
     devocional_id UUID REFERENCES devocionales(id),
-    fecha DATE
+    fecha DATE,
+    semana TEXT
 );
